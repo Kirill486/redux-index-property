4b387a794dad8a2fc8ea702f8a3efe877dc7207f
Kirill Penkin <kirill_486@mail.ru>
2019-05-28 20:50:23 +0400

    simple store (lightweight just to reuse reducer) would work even with thunk

---
+++ src/server/store/SimpleStore.ts
@@ -0,0 +1,29 @@
+export type Reducer<S> = (state: S, action: any) => S;
+
+export class SimpleStore<S> {
+
+    private state: S;
+    private reducer: Reducer<S>;
+
+    constructor(state: S, reducer: Reducer<S>) {
+        this.state = state;
+        this.reducer = reducer;
+    }
+
+    public setState(state: S) {
+        this.state = state;
+    }
+
+    public getState(): S {
+        return this.state;
+    }
+
+    public dispatch(action: any) {
+        if (typeof action === "object") {
+            const newState = this.reducer(this.state, action);
+            this.setState(newState);
+        } else if (typeof action ===  "function") {
+            action(this.dispatch, this.getState); // becouse we have thunk
+        }
+    }
+}
