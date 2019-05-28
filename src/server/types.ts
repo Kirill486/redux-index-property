f97b67bce241c2d697f45e512f3150c90e2369fd
Kirill Penkin <kirill_486@mail.ru>
2019-05-28 19:00:33 +0400

    new task definition

--- src/server/types.ts
+++ src/server/types.ts
@@ -1,4 +1,15 @@
-export interface IApplicationState {
+export interface IUser {
+    name: string;
+    dashboards: IDashboards;
+}
+
+export interface IDashboards { [id: number]: IDashboard; }
+
+export interface IDashboard {
     stars: number;
     comment: string;
+}
+
+export interface IApplicationState {
+    users: IUser;
 }
