8f28e7fa704bd7ab308961762f4e8dbe2f019b0b
Kirill Penkin <kirill_486@mail.ru>
2019-05-28 20:49:01 +0400

    userAction knows dashboardId

--- src/server/store/actions/userAction.ts
+++ src/server/store/actions/userAction.ts
@@ -1,12 +1,14 @@
 import { ActionCreator } from "redux";
-import { IPayloadAction, userActionTypes } from "./actionTypes";
+import { IPayloadAction, userActionTypes, IPayloadIdAction } from "./actionTypes";
 
-export const userAction: ActionCreator<IPayloadAction<IPayloadAction<number | string>>> =
+export const userAction: ActionCreator<IPayloadIdAction<IPayloadAction<number | string>>> =
 (
+    dashboardId: number,
     action: IPayloadAction<number | string>,
 ) => {
     return {
         type: userActionTypes.USER_DASHBOARD_ACTION,
+        id: dashboardId,
         payload: action,
     };
 };
