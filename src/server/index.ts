b8da3c275def618c6f05f0435bd188b348f49b2a
Kirill Penkin <kirill_486@mail.ru>
2019-05-28 19:37:33 +0400

    api nice to have without rewriting previos reducer code

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,7 +1,17 @@
 import { store } from "./store/configureStore";
 import { setStars, setComment } from "./store/actions/modelActions";
 
-// store.dispatch(setStars(3));
+store.dispatch({type: '1'});
 // store.dispatch(setStars(5));
 // store.dispatch(setComment("Oh no!! Who could've known that "));
 // store.dispatch(setComment("they would want a MAP (!its more eficient) of dashboards EACH!"));
+
+// dashboard with id === 7 probably has a misstaken amount
+// of stars - 99
+
+const dashboardId = 7;
+const newValidNumberOfStars = 5;
+const newGoodComment = "now it's ok=)";
+
+store.dispatch(userAction(dashboardId, setStars(newValidNumberOfStars)));
+store.dispatch(userAction(dashboardId, setComment(newGoodComment)));
