4f46d93b28b81a841a808057cfe809fe7e7e326a
Kirill Penkin <kirill_486@mail.ru>
2019-06-16 10:57:27 +0400

    declaration

---
+++ src/redux-index-property/index.ts
@@ -0,0 +1,5 @@
+import { KeyIndexedProperty } from "./lib/KeyIndexedProperty";
+import { PropertyAcceptor } from "./lib/PropertyAcceptor";
+import { LightWeightStore } from "./lib/LightWeightStore";
+
+export { PropertyAcceptor, LightWeightStore, KeyIndexedProperty };
